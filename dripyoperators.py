4200ae6774efc55ebad740729c00f56f44aa3348
miguelthabit <miguelndilahomba@gmail.com>
2025-08-02 19:35:13 +0300

    operators

---
+++ dripyoperators.py
@@ -0,0 +1,9 @@
+print("Using operators")
+math=84
+sst=80
+science=78
+kiswahili=90
+english=90
+total=math+sst+science+kiswahili+english
+print("you are total score is :",total)
+print("your avarage sore is",total/5)
